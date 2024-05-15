c1601c83e32f3a360295539e7dabea9ccc302e81
duthd3 <duthd3@naver.com>
2024-05-15 16:59:53 +0900

    Soongan 프로젝트 생성

---
+++ Soongan/Soongan/SoonganApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoonganApp.swift
+//  Soongan
+//
+//  Created by jun on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SoonganApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
