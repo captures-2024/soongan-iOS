c831e621deff5b8bc27bb46c1ee249eb466c98d9
duthd3 <duthd3@naver.com>
2024-05-16 13:42:01 +0900

    Login -> SignUp 폴더명 및 파일명 변경

---
+++ Soongan/Soongan/SignUp/SignUpFinishView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpFinishView.swift
+//  Soongan
+//
+//  Created by jun on 5/16/24.
+//
+
+import SwiftUI
+
+struct SignUpFinishView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpFinishView()
+}
