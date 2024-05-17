50b0336a80df7bb0dcbc36a1760913318eb421c9
Mini <minnnidev@gmail.com>
2024-05-17 12:32:32 +0900

    [REFACTOR] 진입 뷰 변경

--- Soongan/Soongan/SoonganApp.swift
+++ Soongan/Soongan/SoonganApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct SoonganApp: App {
     var body: some Scene {
         WindowGroup {
-//            SignUpMainView()
-            SignUpView()
+            SignUpMainView()
         }
     }
 }
