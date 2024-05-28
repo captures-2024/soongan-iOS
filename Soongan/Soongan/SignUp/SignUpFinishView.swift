a743030926224d4da347126763ea9d49c6966519
duthd3 <jun.yoon@devercorp.com>
2024-05-28 22:54:52 +0900

    [REFACTOR] SignUpMainView, FinalView 배경 수정

--- Soongan/Soongan/SignUp/SignUpFinishView.swift
+++ Soongan/Soongan/SignUp/SignUpFinishView.swift
@@ -9,15 +9,28 @@ import SwiftUI
 
 struct SignUpFinishView: View {
     var body: some View {
-        Spacer()
-        VStack(spacing: 0) {
-            Text("환영합니다")
+        ZStack {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            BlackView()
+                .edgesIgnoringSafeArea(.all)
+                .opacity(0.5)
             Spacer()
-            Text("김첨지님!")
+            VStack(spacing: 0) {
+                Text("환영합니다")
+                    .foregroundStyle(Color(hex: 0xF5F5F5))
+                Spacer()
+                Text("김첨지님!")
+                    .foregroundStyle(Color(hex: 0xF5F5F5))
+            }
+            .font(.system(size: 36, weight: .semibold))
+            .frame(height: 132)
+            Spacer()
         }
-        .font(.system(size: 36, weight: .semibold))
-        .frame(height: 132)
-        Spacer()
+       
     }
 }
 
